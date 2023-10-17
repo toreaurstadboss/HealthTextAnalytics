359006d866ce40f7c5685b2c6e10784a92e55537
Tore Aurstad <tore.aurstad@gmail.com>
2023-10-17 16:30:03 +0200

    Polling mechanism now in place - returns answer as quickly as possible

--- HealthTextAnalytics/Models/IndexModel.cs
+++ HealthTextAnalytics/Models/IndexModel.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace HealthTextAnalytics.Models
 {
+   
     public class IndexModel
     {
+     
+        [Required]
+        public string InputText { get; set; }
+
         public string AnalysisResult { get; set; }
+
     }
+
 }
